428cc30f32e6ea6f3b1ba7b02ad01d0996ef073c
csgo26 <csgo560@gmail.com>
2024-04-02 01:44:32 +0500

    build database, fetch data from database to Question,jsx

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Data from "../database/Data";
 
 const Question = () => {
   const [checked, setChecked] = useState(undefined);
+
+  const question= Data [0]
+
+  useEffect(( ) =>{
+    console.log(question)
+  })
 
   function onSelect() {
     console.log("radio button change");
@@ -10,24 +17,28 @@ const Question = () => {
   return (
     <>
     
-      <h2 className="text-white pt-9 text-2xl">Simple Question 1</h2>
+      <h2 className="text-white pt-9 text-2xl">{question.question}</h2>
 
-      <ul className="">
-        <li className="pt-9 ">
+      <ul key={question.id}>
+       {
+        question.options.map((q, i) => (
+          <li key={i} className="pt-9 ">
           <input className="  flex-row  "
             type="radio"
             value={checked}
             name="options"
-            id="q1-option"
+            id={`q${i}-option`}
             onChange={onSelect}
             
           />
 
-          <label className="text-white px-2" htmlFor="q1-option ">
-            Option
+          <label className="text-white px-2" htmlFor={`q${i}-option`}>
+            {q}
           </label>
-          <div className="check checked"></div>
+          
         </li>
+        ))
+       }
       </ul>
     </>
   );
